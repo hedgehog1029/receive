ff6f4ab0d9301ee6ab1aeca9c109474ed7bc773f
joshhartigan <joshhartigan99@gmail.com>
2014-10-08 20:08:29 +0100

    Add capability to receive from remote url

--- receive.js
+++ receive.js
@@ -1,13 +1,45 @@
 var request = require('request');
+var program = require('commander');
+var colors = require('colors');
 
-request.get('http://joshhartigan.github.io/rcv.json', // TODO: get urls (obvs)
-    function(err, response, body) {
-      if (!err && response.statusCode == 200) {
-        var json = JSON.parse(body);
+var rcvLog = function(msg) {
+  console.log(" > rcv: ".magenta + msg);
+};
 
-        console.log( 'Installing ' + json['name'] + '...' );
-        console.log( 'by ' + json['author'] );
-      }
+program.version('0.1.0')
+  .usage('<options> <url>')
+  .option('-l, --local', 'Install Thing locally')
+  .option('-g, --global', 'Install Thing globally')
+  .option('-i, --info', 'Get info on Thing without installing')
+  .parse(process.argv);
+
+var thingsRepo = {};
+
+request.get('http://joshhartigan.github.io/receive/things.json',
+  function(err, response, body) {
+    if (!err && response.statusCode == 200) {
+      thingsRepo = JSON.parse(body);
+
+      receiveThing(thingsRepo);
     }
+  }
 );
 
+var receiveThing = function(json) {
+  var thingArg = process.argv[process.argv.length - 1];
+
+  if ( json[thingArg] ) {
+
+    request.get( json[thingArg],
+    function(err, response, body) {
+      if (!err && response.statusCode == 200) {
+        var thingJson = JSON.parse(body);
+
+        rcvLog('Located thing ' + thingJson['name'].bold.red);
+      }
+    });
+
+  } else {
+    rcvLog('Thing ' + thingArg.bold.red + ' not found.');
+  }
+};
